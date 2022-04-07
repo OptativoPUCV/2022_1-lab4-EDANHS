aa53a5b03d31fda04e65c20d1f9e6787f7d997d5
EDANHS <thomas.molina.s@mail.pucv.cl>
2022-04-07 16:14:05 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -93,11 +93,11 @@ void eraseMap(HashMap * map,  char * key) {
 Pair * searchMap(HashMap * map,  char * key) {   
     long i = 0;
     while (is_equal(map->buckets[i]->key,key) != 1){
-        if(i == map->capacity - 1) i = 0;
+        
         i++;
     }
 
-    return map->buckets[i-1];
+    return map->buckets[i];
 }
 
 Pair * firstMap(HashMap * map) {
