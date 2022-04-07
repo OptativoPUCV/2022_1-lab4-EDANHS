e4a57e3bf770aa2c431e2f255ef72aa946fb7810
EDANHS <thomas.molina.s@mail.pucv.cl>
2022-04-07 16:26:27 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -87,6 +87,7 @@ HashMap * createMap(long capacity) {
 
 void eraseMap(HashMap * map,  char * key) {    
     map->buckets[hash(key,map->capacity)]->key = NULL;
+    map->size--;
 }
 
 Pair * searchMap(HashMap * map,  char * key) {   
