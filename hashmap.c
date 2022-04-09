fdf9e52c8eff9f7ed202badd4ec2ef1d58a8b387
EDANHS <thomas.molina.s@mail.pucv.cl>
2022-04-09 08:46:39 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -117,7 +117,7 @@ Pair * nextMap(HashMap * map) {
        if(map->buckets[i] != NULL && map->buckets[i]->key != NULL) break;
        i++;
     }
-    if(i == map->capacity - 1) return NULL;
+    if(i == map->capacity) return NULL;
     map->current = i;
     return map->buckets[i];
 }
